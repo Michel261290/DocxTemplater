8500282f741590f45e1e455fd46007474153a51e
michel261290 <mperez@appwhere.mx>
2018-09-28 11:45:58 -0500

    version stable anexos

--- js/Index.js
+++ js/Index.js
@@ -13,23 +13,23 @@ $("#fileUpload").on('change', function () {
     }
 
     var url = URL.createObjectURL($("#fileUpload").get(0).files[0]);
-
-    var docx = new DocxReader();
     
+    var docx = new DocxReader();
+
     docx.Load(url, function () {
 
         // Buscar Texto
         var found = docx.Search("first"); // Regresa True/False
-        
-        
+
+
         // Remplazar Texto
         docx.Replace("Texto", "Este Texto Fue Cambiado Desde JS");
 
 
         // Cambiar variables en documento
         var docxvar = {
-            "nombre": "Jhon",
-            "apellidoPaterno": "Lopez",
+            "nombre": "Ofirtz",
+            "apellidoPaterno": "Garcia",
             "apellidoMaterno": "Gomez",
             "telefono": "556454664",
             "direccion": "Santa Rosalia 201",
@@ -38,13 +38,39 @@ $("#fileUpload").on('change', function () {
 
         //remplaza variables recibiendo JSON como entrada
         docx.ReplaceVariable(docxvar);
-     
+
 
         // Asigna un nombre al archivo de salida
         docx.SetName("Plantilla.docx")
 
         // Descarga el documento
         docx.Download();
+        $("#fileUpload").val("");
     });
 
 });
+
+
+//Obtiene el base64 de un archivo
+function getBase64(file) {
+    var nameFile = file.name;
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (result) {
+        base64aFile(result.target.result, nameFile);
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
+}
+
+//Obtiene un file a partir de un base64
+function base64aFile(dataurl, filename) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    var file = new File([u8arr], filename, { type: mime });
+    return file;
+}
